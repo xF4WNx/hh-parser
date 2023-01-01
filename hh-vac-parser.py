9b709ece076230a4a38bfcac98221d07e6780454
F4WN <urfriend007.inc@gmail.com>
2023-01-02 00:57:42 +0600

    Допилил до рабочего состояния, осуществляет минимальный функционал скачивания страниц интересующих вакансий добавил комменнты и чутка косметических правок

--- hh-vac-parser.py
+++ hh-vac-parser.py
@@ -11,18 +11,31 @@ def print_hi(name):
 
 
 def get_yaml_config() -> {}:
+    """
+    Загружает в программу конфигурацию через YAML
+    :return: возвращает словарь конфигурации
+    """
     with open('config.yaml', 'r') as yaml_conf:
         return ymlsl(yaml_conf)
 
 
-def get_argv() -> {}:
+def get_argv() -> str:
+    """
+    Считывает из консоли название профессии для её поиска
+    :return: возвращает строку с названием профессии
+    """
     if sys.argv[1:]:
-        return sys.argv[1:]
+        def_profession = ''.join(sys.argv[1:])
+        return def_profession
     else:
         return config['default_profession']
 
 
 def get_user_agent() -> {}:
+    """
+    Создание web заголовков
+    :return: возвращает словарь заголовков
+    """
     headers_def = {
         'User-Agent': UserAgent().random
     }
@@ -30,10 +43,18 @@ def get_user_agent() -> {}:
 
 
 def check_or_create_path():
+    """
+    Проверка на существование директории и создание ее, если она отсутствует
+    """
     Path(f"{config['result_html_path']}{profession}/").mkdir(parents=True, exist_ok=True)
 
 
 def get_page_counts(raw_page: str) -> int:
+    """
+    Парсит количество страниц, которые нужно спарсить из первой страницы
+    :param raw_page: получает сырой текст первой страницы
+    :return: возвращает численное зеначение максимальной страницы ( на hh[.]ru счет страниц начаинается с 0 )
+    """
     soup = bs4.BeautifulSoup(raw_page, 'html5lib')
     counts = []
     for count in soup.find_all('a', {'data-qa': 'pager-page'}):
@@ -42,6 +63,11 @@ def get_page_counts(raw_page: str) -> int:
 
 
 def hh_get_request(page: int) -> str:
+    """
+    Скачаивает запрашиваемую страницу
+    :param page: получает номер нужной страницы
+    :return: возвращает сырой текст страницы
+    """
     req = requests.get(f"{config['hh_link']}{profession}'&page'{page}", headers=headers)
     if req.status_code == 200:
         return req.text
@@ -49,20 +75,32 @@ def hh_get_request(page: int) -> str:
         print(f'Error page, status code: {req.status_code}')
 
 
-def write_response_to_file(page: int, response: str):
+def write_response_to_file(page: int, raw_data: str):
+    """
+    Записывает сырой хтмл в файл
+    :param page: принимает номер страницы
+    :param raw_data: принимает данные
+    """
     with open(f"{config['result_html_path']}{profession}/page {page + 1}.html", 'w', encoding='utf-8') as pars_result:
-        pars_result.write(response)
+        pars_result.write(raw_data)
 
 
 def hh_get_pages_req(max_page: int):
+    """
+    Скачивает номера страницы по номеру кроме первой
+    :param max_page: получает номер страницы
+    """
     for page in range(max_page):
-        response = hh_get_request(page)
-        write_response_to_file(page, response=response)
+        response = str(hh_get_request(page))
+        write_response_to_file(page, raw_data=response)
 
 
 def hh_get_first():
-    response = hh_get_request(0)
-    write_response_to_file(0, response=response)
+    """
+    Скачивает первую страницу и передает на скачаивание остальные
+    """
+    response = str(hh_get_request(0))
+    write_response_to_file(0, raw_data=response)
     max_page = get_page_counts(response) - 1
     hh_get_pages_req(max_page)
 
@@ -75,6 +113,5 @@ if __name__ == '__main__':
 
     check_or_create_path()
     hh_get_first()
-
 else:
     print(f'File not main: {__name__}')
