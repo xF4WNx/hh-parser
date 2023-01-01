05e1d391534392761be5450293b033d95eb34044
F4WN <urfriend007.inc@gmail.com>
2023-01-02 00:33:40 +0600

    Допилил до рабочего состояния, осуществляет минимальный скачивание страниц интересующих вакансий

--- hh-vac-parser.py
+++ hh-vac-parser.py
@@ -1,30 +1,32 @@
 import sys
-import os
 import bs4
 import requests
 from fake_useragent import UserAgent
 from pathlib import Path
-from yaml import safe_load as YMLSL
+from yaml import safe_load as ymlsl
 
 
 def print_hi(name):
     print(f'{name} started')
 
 
-def get_yaml_config():
+def get_yaml_config() -> {}:
     with open('config.yaml', 'r') as yaml_conf:
-        return YMLSL(yaml_conf)
+        return ymlsl(yaml_conf)
 
 
-def get_argv():
+def get_argv() -> {}:
     if sys.argv[1:]:
         return sys.argv[1:]
     else:
         return config['default_profession']
 
 
-def get_user_agent() -> str:
-    return UserAgent().random
+def get_user_agent() -> {}:
+    headers_def = {
+        'User-Agent': UserAgent().random
+    }
+    return headers_def
 
 
 def check_or_create_path():
@@ -34,39 +36,45 @@ def check_or_create_path():
 def get_page_counts(raw_page: str) -> int:
     soup = bs4.BeautifulSoup(raw_page, 'html5lib')
     counts = []
-    for count in soup.find_all('a', {'data-qa':'pager-page'}):
+    for count in soup.find_all('a', {'data-qa': 'pager-page'}):
         counts.append(int(count.text))
     return max(counts)
 
 
-def hh_get_pages_req(max_page: int):
-    ...
-
-
-def hh_get_first_req():
-    headers = {
-        'User-Agent': get_user_agent()
-    }
-
-    req = requests.get(config['hh_link'] + profession, headers=headers)
+def hh_get_request(page: int) -> str:
+    req = requests.get(f"{config['hh_link']}{profession}'&page'{page}", headers=headers)
     if req.status_code == 200:
-        req_text = req.text
-        with open(config['result_html_path'] + 'page 1.html', 'w', encoding='utf-8') as pars_result:
-            pars_result.write(req_text)
-        max_page = get_page_counts(req_text) - 1
-        hh_get_pages_req(max_page)
+        return req.text
     else:
         print(f'Error page, status code: {req.status_code}')
 
 
+def write_response_to_file(page: int, response: str):
+    with open(f"{config['result_html_path']}{profession}/page {page + 1}.html", 'w', encoding='utf-8') as pars_result:
+        pars_result.write(response)
+
+
+def hh_get_pages_req(max_page: int):
+    for page in range(max_page):
+        response = hh_get_request(page)
+        write_response_to_file(page, response=response)
+
+
+def hh_get_first():
+    response = hh_get_request(0)
+    write_response_to_file(0, response=response)
+    max_page = get_page_counts(response) - 1
+    hh_get_pages_req(max_page)
+
+
 if __name__ == '__main__':
     print_hi('HH_vacancy_parser')
-    print(f'{os.listdir()}')
     config = get_yaml_config()
     profession = get_argv()
+    headers = get_user_agent()
 
     check_or_create_path()
-    hh_get_first_req()
+    hh_get_first()
 
 else:
     print(f'File not main: {__name__}')
